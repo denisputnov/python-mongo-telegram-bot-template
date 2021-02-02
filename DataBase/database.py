dbfad853bd10d9238b10d212f19346beaec650cf
grnbows <grnbows.connect@mail.ru>
2021-02-02 14:58:35 +0300

    added many else functions and docs

--- DataBase/database.py
+++ DataBase/database.py
@@ -1,6 +1,19 @@
 from pymongo import MongoClient
 
+
 class DataBase:
+    """
+    - register_user(message)
+    - subscribe_user(message)
+    - unsubscribe_user(message)
+    - check_subscription_status_for_user(user_id)
+    - get_all_users_data()
+    - get_subscribed_users_data()
+    - get_all_users_identificators()
+    - get_subscribed_users_identificators()
+    - get_subscription_stats()
+    """
+
     def __init__(self, db_user, db_password, db_name):
         self._cluster = MongoClient(f'mongodb+srv://{db_user}:{db_password}@cluster0.gafny.mongodb.net/{db_name}?retryWrites=true&w=majority')
         self._db = self._cluster[db_name]
@@ -13,7 +26,53 @@ class DataBase:
         print('db inited successfully')
     
 
+    def register_user(self, message):
+        """ 
+        register user in the colelction[users] in the format: 
+        {
+            '_id': message.from_user.id,
+            'username': message.chat.username,
+            'first_name': message.from_user.first_name,
+            'last_name': message.from_user.last_name,
+            'isSubscribed': False,
+        }
+        """
+        try:
+            # if user is not registered
+            if self._users_collection.count_documents({'_id': message.from_user.id}) == 0:
+                self._users_collection.insert_one({
+                    '_id': message.from_user.id,
+                    'username': message.chat.username,
+                    'first_name': message.from_user.first_name,
+                    'last_name': message.from_user.last_name,
+                    'isSubscribed': False,
+                })
+            # if user were registered before
+            else: 
+                print(f'Tried to register user(_id: {message.from_user.id}), but he were registered before.')
+
+            # return True if operation is done
+            return True
+
+        except Exception as error:
+            # return False if operation is not done
+            print(error)
+            return False
+
+
     def subscribe_user(self, message):
+        """ 
+        subscribe user in the collection[users]
+        if user is not in the collection, add user in the format:
+        {
+            '_id': message.from_user.id,
+            'username': message.chat.username,
+            'first_name': message.from_user.first_name,
+            'last_name': message.from_user.last_name,
+            'isSubscribed': True,
+        }
+        if user is alreary in the collection, change user field "isSubscribed" = True
+        """
         try:
             # add user if user is not in collection
             if self._users_collection.count_documents({'_id': message.from_user.id}) == 0:
@@ -38,6 +97,18 @@ class DataBase:
 
 
     def unsubscribe_user(self, message):
+        """ 
+        unsubscribe user from the collection[users]
+        if user is not in the collection, add user in the format:
+        {
+            '_id': message.from_user.id,
+            'username': message.chat.username,
+            'first_name': message.from_user.first_name,
+            'last_name': message.from_user.last_name,
+            'isSubscribed': False,
+        }
+        if user is alreary in the collection, change user field "isSubscribed" = False
+        """
         try:
             # add user if user is not in collection
             if self._users_collection.count_documents({'_id': message.from_user.id}) == 0:
@@ -61,35 +132,67 @@ class DataBase:
             return False
             
 
-    def register_user(self, message):
+    def check_subscription_status_for_user(self, user_id):
+        """
+        Check user subscription status;
+        return True/False values;
+        """
         try:
-        # if user is not registered
-            if self._users_collection.count_documents({'_id': message.from_user.id}) == 0:
-                self._users_collection.insert_one({
-                    '_id': message.from_user.id,
-                    'username': message.chat.username,
-                    'first_name': message.from_user.first_name,
-                    'last_name': message.from_user.last_name,
-                    'isSubscribed': False,
-                })
-            # if user were registered before
-            else: 
-                print(f'Tried to register user(_id: {message.from_user.id}), but he were registered before.')
-
-            # return True if operation is done
-            return True
+            return self._users_collection.find_one({"_id": user_id})['isSubscribed'] 
 
         except Exception as error:
-            # return False if operation is not done
             print(error)
-            return False
 
 
-    def get_all_users(self):
+    def get_all_users_data(self):
+        """
+        return users list with all data from collection[users]
+        list(dict(), dict(), dict(),...)
+        """
         return list(self._users_collection.find())
     
 
+    def get_subscribed_users_data(self):
+        """
+        return users list with all data from collection[users] for subscriber users [isSubscribed == True]
+        list(dict(), dict(), dict(),...)
+        """
+        users = self.get_all_users_data()
+        return [user for user in users if user['isSubscribed'] == True]
+
+
     def get_all_users_identificators(self): 
+        """
+        return list with only users identificators from collection[users]
+        lust(int, int, int,...)
+        """
         users = list(self._users_collection.find())
         return [user['_id'] for user in users]
 
+
+    def get_subscribed_users_identificators(self):
+        """
+        return list with only users identificators from collection[users] for subscriber users [isSubscribed == True]
+        lust(int, int, int,...)
+        """
+        users = self.get_all_users_data()
+        return [user['_id'] for user in users if user['isSubscribed'] == True]
+
+
+    def get_subscription_stats(self):
+        """
+        return {
+            'percentage': [float] value from 0 to 1 which means percentage of subscription by users
+            'subscribed': [int] amount of subscribed users,
+            'total': [int] total amount of interacted users,
+        }
+        """
+        users = self.get_all_users_data()
+        subscribed = len([user for user in users if user['isSubscribed'] == True])
+        total = len(users)
+
+        return {
+            'percentage': subscribed / total,
+            'subscribed': subscribed,
+            'total': total,
+        }
--- bot.py
+++ bot.py
@@ -31,7 +31,25 @@ def test1(message):
 
 @bot.message_handler(commands=['get_all_users'])
 def test2(message):
-    msg = ' '.join(map(str, database.get_all_users()))
+    msg = ' '.join(map(str, database.get_all_users_data()))
     bot.send_message(message.from_user.id, msg)
+
+@bot.message_handler(commands=['get_subscribed_users_data'])
+def test10(message):
+    msg = ' '.join(map(str, database.get_subscribed_users_data()))
+    bot.send_message(message.from_user.id, msg)
+
+@bot.message_handler(commands=['get_subscribed_users_identificators'])
+def test15(message):
+    msg = ' '.join(map(str, database.get_subscribed_users_identificators()))
+    bot.send_message(message.from_user.id, msg)
+
+@bot.message_handler(commands=['check_my_status'])
+def test3(message):
+    bot.send_message(message.from_user.id, database.check_subscription_status_for_user(message.chat.id))
+
+@bot.message_handler(commands=['stats'])
+def test4(message):
+    bot.send_message(message.from_user.id, str(database.get_subscription_stats()))
 
 bot.polling(none_stop=True)
