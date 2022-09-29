22198ac571793a15d9a4b26c9129385f2c21bdd7
Jobians Techie <88005779+Jobians@users.noreply.github.com>
2022-09-29 20:50:12 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,8 @@
 import telebot
 import config
+import dns.resolver
+dns.resolver.default_resolver=dns.resolver.Resolver(configure=False)
+dns.resolver.default_resolver.nameservers=['8.8.8.8']
 from DataBase.database import DataBase
 
 bot = telebot.TeleBot(config.TOKEN)
